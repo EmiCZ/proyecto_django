d6eb455373aa2a0e8842719db904e9691d4404c9
EmiCZ <capurrozanandrea@gmail.com>
2022-10-13 23:34:36 -0300

    Se agrego app blog y template

--- ProyectoDjango/settings.py
+++ ProyectoDjango/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'AppCoder.apps.AppcoderConfig'
+    'AppCoder.apps.AppcoderConfig',
+    'blog.apps.BlogConfig'
 ]
 
 MIDDLEWARE = [
